efe66c2c86b5cf50efe17c9a60508f652301731f
Sushmita125 <68917656+Sushmita125@users.noreply.github.com>
2021-07-25 01:53:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def getAllData():
     #Return JSON
     return jsonify(Age=ages, Emotion=emotions, Gender=genders)
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=80, ssl_context=('cert.pem', 'key.pem')
+    app.run(host='0.0.0.0', port=80, ssl_context=('cert.pem', 'key.pem'))
